8bfad1866a9c66e38738c6c4e821f52991ce566f
Filippov-Nikolay <filippovnikolay195@gmail.com>
2022-07-09 16:08:43 +0300

    main

--- js/main.js
+++ js/main.js
@@ -5,14 +5,13 @@ $(function(){
         autoplay: 3000,
         autoplaySpeed: 2000,
         dots: true
-    })
+    });
+
+    $('.nav__button').on('click', function() {
+        $('.header__menu-burger').addClass('active');
+    });
+    $('.menu-close').on('click', function() {
+        $('.header__menu-burger').removeClass('active');
+    });
 });
 
-function menu() {
-    let menuDisplay = document.getElementById('menu');
-    if (menuDisplay.style.display == 'none') {
-        menuDisplay.style.display = 'block';
-    } else {
-        menuDisplay.style.display = 'none';
-    }
-}
